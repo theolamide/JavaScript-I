77208f8edfb2a14d1e0dbb67f2f87406b204083a
Daniel Ajadi <ajadidan@gmail.com>
2019-09-16 20:41:25 -0400

    Objects MVP done

--- assignments/objects.js
+++ assignments/objects.js
@@ -91,6 +91,7 @@ console.log(intern2.hello())
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
 console.log(intern5.multiply(4,15));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
